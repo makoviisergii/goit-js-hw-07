d7c6780714310d0e4480ab72ceeb015f40322824
makoviisergii <sergiimakovii78@gmail.com>
2023-01-29 23:47:42 +0200

    add7.2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector("ul.gallery");
+const galleryArr = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("beforeend", galleryArr);
+
+function openOrigImag(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {});
